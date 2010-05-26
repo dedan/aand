7052aaec77c0454bca3657199eebc79190ccaf4e
RAF <raf@ICKE.(none)>
2010-05-26 17:19:45 +0200

    sheet 4 complete

--- prac4/generatePoissonTrains.m
+++ prac4/generatePoissonTrains.m
@@ -1,46 +1,4 @@
-function generatePoissonTrains
-
-%% author: Paula Kuokkanen
-%% 5. Mai 2009
-
-%% homogeneous Poisson process
-
-% A homogeneous Poisson process generates events (spikes) with a constant 
-% probability or rate r(t) = r. Spikes are independent. Thus, at each 
-% time point t, the probability P of observing a spike within 
-% a sufficiently small temporal window from t to t + delta_t is 
-% P= r*delta_t.
-
-% There are two simple ways of implementing a homogeneous Poisson process:
-% 1.    Progress through time in small steps of size delta_t, 
-%       draw a random number x (from a uniform distribution 
-%       between 0 and 1, Matlab function 'rand' 
-%       at each time step. If x < P, generate a spike.
-%
-% 2.    Generate interspike intervals from an exponential probability 
-%       density. When x is uniformly distributed between 0 and 1, 
-%       its negative logarithm is exponentially distributed. Thus, we 
-%       can generate spike times t(i) iteratively from the 
-%       formula t(i+1) = t(i) - log(x)/r.
-
-% Using implementation #2, generate 1000 spikes by a homogeneous
-% Poisson process with a rate of r=100 (Hz)
-
-n = 1000;   % desired number of spikes
-rate = 100;  % rate in spikes per second
-randNumbers = rand(n,1);  % n random numbers, uniform distribution
-ISIs = -log(randNumbers)/rate;   % ISIs in seconds
-ISIs = ISIs*1000;   % ISIs in milliseconds
-
-% compute spike times from ISIs
-
-SpikeTimes = zeros(n,1);
-SpikeTimes(1) = ISIs(1);
-for i = 2: length(SpikeTimes)
-    SpikeTimes(i) = SpikeTimes(i-1)+ISIs(i);
-end
-
-
+function SpikeTimes_inh = generatePoissonTrains(rates, len)
 %% inhomogeneous Poisson process
 
 % An inhomogeneous Poisson process is characterized by a time-dependent
@@ -68,8 +26,8 @@ end
 % First, start with the homogeneous, but with more spikes so that you can
 % thin out later on
 
-n = 10000;   % desired number of spikes
-maxRate = 150;%150;  % rate in spikes per second
+n = length(rates);   % desired number of spikes
+maxRate = max(rates); %150;  % rate in spikes per second
 randNumbers2 = rand(n,1);  % n random numbers, uniform distribution
 ISIs2 = -log(randNumbers2)/maxRate;   % ISIs in seconds
 ISIs2 = ISIs2*1000;   % ISIs in milliseconds
@@ -86,58 +44,10 @@ end
 amp = 100; % 50; %
 fre = 10;% 10;
 randNumbers = rand(n,1);
-rates = amp*sin(SpikeTimes2*2*pi*fre./1000) + 100;
 Ps = rates/maxRate;
 SpikeTimes_inh = SpikeTimes2(find(randNumbers>Ps));
-n = 1000;
+n = len;
 SpikeTimes_inh = SpikeTimes_inh(1:n);     % take just 1000 spikes
 
-%% Poisson process with absolute refractory period
+%save('SpikeTimes_inh')
 
-% Real neurons are in a refractory state immediately after a spike. 
-% Thus, during a certain ‘refractory period’ after a spike, it is 
-% impossible or much harder to evoke the next spike.
-% When the effect of refractoriness is taken into account, spikes 
-% can no longer considered to be independent because the probability 
-% of observing the next spike then also depends on the time that has 
-% passed since the last spike.
-%
-% In point process models, an approximation of refractoriness is to 
-% set the probability of generating a spike to 0 for a certain absolute 
-% refractory period tr after a spike, and then to r afterwards. 
-%
-% We simulate a homogeneous Poisson process and add an absolute refractory
-% period of t_r = 5 ms. We use different ‘driving’ rates 
-% r = 10, 50, 100, 200, 500, and 1000Hz.
-% We simulate 1000 spikes for each rate r.
-%
-% Adding an absolute refractory period corresponds to simply adding a 
-% fixed value to each interspike interval of a ‘normal’ 
-% homogeneous Poisson process.
-
-
-n = 1000;   % desired number of spikes
-
-rates_ref = [10 50 100 200 500 1000];  % "driving" rates in spikes per second
-ISIs_ref = zeros(n,length(rates_ref));
-randNumbers3 = rand(n,1);  % n random numbers, uniform distribution
-t_r = 5;    % absolute refractory period in ms
-
-for i = 1:length(rates_ref)
-    ISIs_ref(:,i) = -log(randNumbers3)/rates_ref(i)*1000 + t_r;   % ISIs in milliseconds
-end
-
-% compute spike times from ISIs
-
-SpikeTimes_ref = zeros(n,length(rates_ref));
-for j = 1:length(rates_ref)
-    SpikeTimes_ref(1,j) = ISIs_ref(1,j);
-    for i = 2:n
-        SpikeTimes_ref(i,j) = SpikeTimes_ref(i-1,j)+ISIs_ref(i,j);
-    end
-end
-
-
-save('MyPoissonSpikeTrains.mat','SpikeTimes','SpikeTimes_inh','SpikeTimes_ref','rates_ref','SpikeTimes_refT')
-
-clear
