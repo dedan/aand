1c4c896c16cb3a3832e611d24c4507ad51ec6e0c
RAF <raf@ICKE.(none)>
2010-06-16 13:48:19 +0200

    labels prac7

--- prac6/fisher.m
+++ prac6/fisher.m
@@ -5,6 +5,7 @@ class2 = data(:,logical(classes(2,:)));
 
 u1 = mean(class1,2);
 u2 = mean(class2,2);
+
 s1 = zeros(size(class1,1));
 s2 = zeros(size(class2,1));
 
@@ -12,7 +13,6 @@ for i = 1:size(class1,2)
     s1 = s1 + (class1(:,i) - u1) * (class1(:,i) - u1)'; 
 end
     
-
 for i = 1:size(class2,2)
     s2 = s2 + (class2(:,i) - u2) * (class2(:,i) - u2)'; 
 end
